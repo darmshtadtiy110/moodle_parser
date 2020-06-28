32f21c6bb19becfe4d71f7d0a25656a4590fc041
darmshtadtiy110 <vdovinbogdan0@gmail.com>
2020-06-28 14:39:50 +0300

    extending resources by student

--- Classes/Resources/Student.php
+++ Classes/Resources/Student.php
@@ -4,81 +4,119 @@
 namespace Resources;
 
 
+use Exception;
+use Factory\PassportFactory;
 use General\Passport;
 use FileSystem\Cookies;
+use General\Signal;
+use Parser\StudentParser;
+use Request\CurlErrorException;
+use Request\Login;
 
-
-class Student
+class Student extends Resource
 {
-	/** @var Passport  */
+	use ParentResource;
+
+	/** @var Student */
+	private static $instance;
+
+	/** @var StudentParser */
+	private $parser;
+
+	/** @var Passport */
 	private $passport;
-
-	/** @var string */
-	private $name;
-
-	/** @var array */
-	private $course_list;
 
 	/** @var Cookies */
 	private $cookies;
 
+	/** @var array */
+	private $course_list = [];
+
 	/**
-	 * Student constructor.
-	 * @param Passport $passport
-	 * @param $name
-	 * @param $course_list
-	 * @param $cookies
+	 * @param null | int $passport_id
+	 * @return Student
 	 */
-	public function __construct(Passport $passport, $name, $course_list, $cookies)
+	public static function getInstance($passport_id = null)
 	{
-		$this->passport = $passport;
-		$this->name = $name;
-		$this->course_list = $course_list;
-		$this->cookies = $cookies;
+		if( empty(self::$instance) )
+		{
+			$user_passport = PassportFactory::create($passport_id);
+
+			$student = new Student();
+			$student->passport($user_passport);
+			$student->auth();
+			self::$instance = $student;
+		}
+
+		return self::$instance;
 	}
 
 	/**
-	 * @return mixed
+	 * @param string $name
+	 * @return string
 	 */
-	public function passport()
+	public function name($name = "")
 	{
-		return $this->passport;
-	}
+		if($name != "")
+			$this->name = $name;
 
-	/**
-	 * @return mixed
-	 */
-	public function name()
-	{
 		return $this->name;
+	}
+
+	/**
+	 * @param Passport | null $passport
+	 * @return Passport | bool
+	 */
+	public function passport(Passport $passport = null)
+	{
+		if( $passport )
+		{
+			$this->passport = $passport;
+		}
+
+		return $this->passport;
 	}
 
 	/**
 	 * @return Cookies
 	 */
-	public function getCookies()
+	public function cookies()
 	{
+		if( empty($this->cookies) )
+			$this->cookies = new Cookies($this->passport()->login().".txt");
+
 		return $this->cookies;
 	}
 
-	/**
-	 * @return mixed
-	 */
-	public function getCourseList()
+
+	public function auth()
 	{
-		return $this->course_list;
+
+		// TODO add id parsing
+		try {
+			$login_request = new Login(
+				$this->passport()->login(),
+				$this->passport()->password(),
+				$this->cookies()
+			);
+
+			$this->parser()->setParsePage($login_request->response);
+
+			if($this->parser()->getLoginResults() === true)
+			{
+				try {
+					$this->name = $this->parser()->getUserText();
+
+					$this->course_list = $this->parser()->getCourseList();
+				}
+				catch (Exception $e) {
+					echo $e->getMessage();
+				}
+
+			}
+			else { Signal::msg( $this->parser->getLoginError()); };
+
+		}
+		catch(CurlErrorException $e) { Signal::msg($e->getMessage()); }
 	}
-
-	public function getCourse($id)
-	{
-		return $this->course_list[$id];
-	}
-
-	public function loadResource(Resource $resource)
-	{
-		$resource->loadFromParser($this->getCookies());
-	}
-
-
-
 }
