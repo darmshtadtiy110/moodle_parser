62e4bc73e859688764bb893c0f3cf495c25a2192
darmshtadtiy110 <vdovinbogdan0@gmail.com>
2020-06-29 16:55:05 +0300

    Make Attempt a parent resource

--- Classes/Resources/Attempt.php
+++ Classes/Resources/Attempt.php
@@ -4,23 +4,7 @@
 namespace Resources;
 
 
-use Parser\Parser;
-
-class Attempt extends Resource
+abstract class Attempt extends Resource
 {
-	/** @var array */
-	protected $question_list = [];
-
-	/** @var integer */
-	protected $grade;
-
-	public function getGrade()
-	{
-		return $this->grade;
-	}
-
-	public function parserLoader(Parser $parser)
-	{
-		// TODO: Implement parserLoader() method.
-	}
+	use ParentResource;
 }
