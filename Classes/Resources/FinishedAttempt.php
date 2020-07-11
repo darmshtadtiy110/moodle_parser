717723b586699e1f2fc552eb99786cbe123a016e
darmshtadtiy110 <vdovinbogdan0@gmail.com>
2020-07-11 16:44:38 +0300

    Explicit parser setup

--- Classes/Resources/FinishedAttempt.php
+++ Classes/Resources/FinishedAttempt.php
@@ -4,12 +4,19 @@
 namespace Resources;
 
 
-//use Parser\Resources\FinishedAttemptParser;
+use Parser\Resources\FinishedAttemptParser;
 
 class FinishedAttempt extends Attempt
 {
 	/** @var int */
 	private $grade;
+
+	public function __construct($id, $grade, $name = "")
+	{
+		$this->grade = $grade;
+		$this->parser = new FinishedAttemptParser();
+		parent::__construct($id, $name);
+	}
 
 	/**
 	 * @return mixed
