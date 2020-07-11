8cba1903831661357282dcdefa446df24abea491
darmshtadtiy110 <vdovinbogdan0@gmail.com>
2020-07-11 16:09:54 +0300

    Explicit parser setup

---
+++ Classes/Parser/Resources/DocumentParser.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Parser\Resources;
+
+
+class DocumentParser
+{
+
+}
--- Classes/Resources/Document.php
+++ Classes/Resources/Document.php
@@ -4,9 +4,17 @@
 namespace Resources;
 
 use General\Resource;
+use Parser\Resources\DocumentParser;
 
 class Document extends Resource
 {
+	public function __construct($id, $name = "")
+	{
+		$this->parser = new DocumentParser;
+
+		parent::__construct($id, $name);
+	}
+
 	protected function use_parser()
 	{
 		// TODO: Implement parserLoader() method.
