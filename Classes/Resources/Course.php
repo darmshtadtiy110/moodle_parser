7e8a2c1c517ca6a2fd8837c47168cb1bee2511bc
darmshtadtiy110 <vdovinbogdan0@gmail.com>
2020-07-11 16:12:15 +0300

    Explicit parser setup, remove nonsensical foreach

--- Classes/Resources/Course.php
+++ Classes/Resources/Course.php
@@ -4,24 +4,63 @@
 namespace Resources;
 
 use General\Resource;
-use Interfaces\ParentResource;
+use Parser\Resources\CourseParser;
 
-use Traits\ParentUtilities;
-
-class Course extends Resource implements ParentResource
+class Course extends Resource
 {
-	use ParentUtilities;
-
 	private $quiz_list = [];
 
 	private $document_list = [];
 
+	public function __construct($id)
+	{
+		$this->parser = new CourseParser();
+		parent::__construct($id);
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getQuizList()
+	{
+		return $this->quiz_list;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getDocumentList()
+	{
+		return $this->document_list;
+	}
+
+	public function getQuiz($id)
+	{
+		if(array_key_exists($id, $this->quiz_list))
+			return $this->quiz_list[$id];
+		return false;
+	}
+
+	public function getDocument($id)
+	{
+		if(array_key_exists($id, $this->document_list))
+			return $this->document_list[$id];
+		return false;
+	}
+
+	public function setQuiz(Quiz $quiz)
+	{
+		$this->quiz_list[$quiz->getId()] = $quiz;
+	}
+
+	public function setDocument(Document $doc)
+	{
+		$this->document_list[$doc->getId()] = $doc;
+	}
+
 	protected function use_parser()
 	{
-		foreach ( $this->parser()->getQuizList() as $quiz)
-		{
-			$this->setChild($quiz);
-		}
+		$this->quiz_list = $this->parser->getQuizList();
 	}
 
 }
