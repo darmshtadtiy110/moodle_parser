6c2547e31619d1c96b5e6ee95f28c22116308d77
darmshtadtiy110 <vdovinbogdan0@gmail.com>
2020-07-11 17:44:07 +0300

    Add static Auth function

--- Classes/Resources/Student.php
+++ Classes/Resources/Student.php
@@ -4,8 +4,8 @@
 namespace Resources;
 
 
-use Exception;
-
+use General\Exceptions\CurlErrorException;
+use General\Properties;
 use General\Signal;
 use General\Request;
 use General\Passport;
@@ -16,19 +16,10 @@ use Factory\PassportFactory;
 
 use Parser\Resources\StudentParser;
 
-use Interfaces\ParentResource;
-
-use Traits\ParentUtilities;
-
-class Student extends Resource implements ParentResource
+class Student extends Resource
 {
-	use ParentUtilities;
-
 	/** @var Student */
 	private static $instance;
-
-	/** @var StudentParser */
-	protected $parser;
 
 	/** @var Passport */
 	private $passport;
@@ -36,8 +27,15 @@ class Student extends Resource implements ParentResource
 	/** @var Cookies */
 	private $cookies;
 
-	/** @var array */
+	/** @var Course[] */
 	private $course_list = [];
+
+	public function __construct($id, $name = "", $course_list = [])
+	{
+		$this->course_list = $course_list;
+		$this->parser = new StudentParser();
+		parent::__construct($id, $name);
+	}
 
 	/**
 	 * @param int $passport_id
@@ -45,42 +43,65 @@ class Student extends Resource implements ParentResource
 	 */
 	public static function getInstance($passport_id = null)
 	{
-		if( $passport_id > 0)
+		if($passport_id > 0)
 		{
 			$user_passport = PassportFactory::create($passport_id);
 
-			$student = new Student();
-			$student->passport($user_passport);
-
-			self::$instance = $student;
+			self::$instance = self::Auth($user_passport);
 		}
 
 		return self::$instance;
 	}
 
-	/**
-	 * @param string $name
-	 * @return string
-	 */
-	public function name($name = "")
+	public static function Auth(Passport $passport)
 	{
-		if($name != "")
-			$this->name = $name;
+		$parser = new StudentParser();
 
-		return $this->name;
+		$login_request = Request::Login(
+			$passport->login(),
+			$passport->password()
+		);
+
+		$parser->setParsePage($login_request->response());
+
+		if($parser->getLoginResults() === true)
+		{
+			$name = $parser->getUserText();
+			$course_list = $parser->getCoursesArray();
+			$id = $parser->getUserId();
+
+			return new Student($id, $name, $course_list);
+		}
+		else { Signal::msg( $parser->getLoginError()); };
+		return false;
 	}
 
 	/**
-	 * @param Passport | null $passport
+	 * @return array
+	 */
+	public function getCourseList()
+	{
+		return $this->course_list;
+	}
+
+	/**
+	 * @param $id
+	 * @return bool|Course
+	 */
+	public function getCourse($id)
+	{
+		if(array_key_exists($id, $this->course_list))
+		{
+			return $this->course_list[$id];
+		}
+		return false;
+	}
+
+	/**
 	 * @return Passport | bool
 	 */
-	public function passport(Passport $passport = null)
+	public function passport()
 	{
-		if( $passport )
-		{
-			$this->passport = $passport;
-		}
-
 		return $this->passport;
 	}
 
@@ -89,40 +110,25 @@ class Student extends Resource implements ParentResource
 	 */
 	public function cookies()
 	{
-		if( empty($this->cookies) )
-			$this->cookies = new Cookies($this->passport()->login().".txt");
+		if (empty($this->cookies))
+			$this->cookies = new Cookies($this->passport()->login() . ".txt");
 
 		return $this->cookies;
 	}
 
-	public function auth()
-	{
-		$this->parse();
-	}
-
 	protected function getParsablePage()
 	{
-		$login_request = Request::Login(
-			$this->passport()->login(),
-			$this->passport()->password()
-		);
-		$this->parser()->setParsePage($login_request->response());
+		try {
+			$user_profile_request = new Request(
+				Properties::Profile() . $this->id
+			);
+			$this->parser()->setParsePage($user_profile_request->response());
+		}
+		catch (CurlErrorException $e) {}
 	}
 
 	protected function use_parser()
 	{
-		if($this->parser->getLoginResults() === true)
-		{
-			try {
-				$this->name = $this->parser->getUserText();
-
-				$this->course_list = $this->parser->getCoursesArray();
-			}
-			catch (Exception $e) {
-				echo $e->getMessage();
-			}
-
-		}
-		else { Signal::msg( $this->parser->getLoginError()); };
+		//TODO
 	}
 }
