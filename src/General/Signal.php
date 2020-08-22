f943b53503cb3efab237d6474e4310cb0f2d53fc
darmshtadtiy110 <vdovinbogdan0@gmail.com>
2020-08-22 11:44:05 +0300

    Replace echo with log

--- src/General/Signal.php
+++ src/General/Signal.php
@@ -10,11 +10,21 @@ class Signal
 
 	public static function login_successful($name)
 	{
-		echo self::$login_successful.$name."\n";
+		//echo self::$login_successful.$name."\n";
+		self::log($name);
+
 	}
 
 	public static function msg($msg)
 	{
-		echo $msg."\n";
+		//echo $msg."\n";
+		self::log($msg);
+	}
+
+	public static function log($msg)
+	{
+		$file = fopen("../../storage/logs/moodle_parser.log", "w+");
+
+		file_put_contents($file, $msg);
 	}
 }
